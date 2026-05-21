67ffdd7df18561e117b9f70ce2ee1b073d59a9dd
undrnz-dazzl-sky <underneathdazzlingsky@gmail.com>
2026-05-21 20:08:07 +0300

    build a custom sign-in page

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,14 +1,26 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import Google from "next-auth/providers/google";
 
 const authConfig = {
-    providers: [
-        Google({
+    providers:
+        [Google({
             clientId: process.env.AUTH_GOOGLE_ID,
             clientSecret: process.env.AUTH_GOOGLE_SECRET,
         }),
-    ],
+        ],
+    callbacks: {
+        authorized({ auth, request }) {
+            return !!auth?.user
+        },
+    },
+    pages: {
+        signIn: "/login",
+    },
 };
 
-export const { auth, handlers: { GET, POST },
+export const {
+    auth,
+    signIn,
+    signOut,
+    handlers: { GET, POST },
 } = NextAuth(authConfig);
